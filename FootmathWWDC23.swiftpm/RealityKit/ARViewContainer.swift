f9c123e1d90bf2f3e372a33f33531eef4e3c52b8
Guilherme Lenzolari <103944988+guilenzolari@users.noreply.github.com>
2024-01-29 14:52:41 -0300

    faz numero dos botoes atualizarem quando a array de palpites muda

--- FootmathWWDC23.swiftpm/RealityKit/ARViewContainer.swift
+++ FootmathWWDC23.swiftpm/RealityKit/ARViewContainer.swift
@@ -36,6 +36,7 @@ struct ARViewContainer: UIViewRepresentable {
             let buttonsEntity = try! ModelEntity.load(named: "botaoGol\(index + 1)")
             buttonsArrayEntity.append(buttonsEntity)
             let anchorButtons = AnchorEntity(.plane(.horizontal, classification: [.floor, .table], minimumBounds: [0.2, 0.2]))
+            anchorButtons.name = "Buttons"
             buttonsArrayEntity[index].scale = [0.05, 0.05, 0.05]
             buttonsArrayEntity[index].generateCollisionShapes(recursive: true)
             anchorButtons.addChild(buttonsArrayEntity[index])
@@ -48,6 +49,8 @@ struct ARViewContainer: UIViewRepresentable {
             let anchorPalpiteText = AnchorEntity(.plane(.horizontal, classification: [.floor, .table], minimumBounds: [0.2, 0.2]))
             textEntity.scale = SIMD3<Float>(x: 0.03, y: 0.03, z: 0.1)
             textEntity.generateCollisionShapes(recursive: true)
+            textEntity.name = "\(index)"
+            anchorPalpiteText.name = "Palpite-\(index)"
             anchorPalpiteText.addChild(textEntity)
             anchorPalpiteText.position = SIMD3<Float>(x: Float(numbersPosition[index][0]), y: Float(numbersPosition[index][1]), z: Float(numbersPosition[index][2]))
             arView.scene.addAnchor(anchorPalpiteText)
@@ -58,13 +61,45 @@ struct ARViewContainer: UIViewRepresentable {
     }
     
     //from SWiftUI to UIKit
-    func updateUIView(_ uiView: ARView, context: Context) {
+    func updateUIView(_ arView: ARView, context: Context) {
+        
+        let anchorTextPalpites = arView.scene.anchors.filter { element in
+            element.name.contains("Palpite")
+        }
+        
+        //Recria o texto
+        for anchorTextPalpite in anchorTextPalpites {
+            if anchorTextPalpite.children.count > 0 {
+                let child = anchorTextPalpite.children[0]
+                
+                if let index = Int(child.name) {
+                    anchorTextPalpite.removeChild(child)
+                    let textMesh = MeshResource.generateText("\(palpites[index])", extrusionDepth: 0.1, font: .systemFont(ofSize: 2))
+                    
+                    let textMaterial = SimpleMaterial(color: .black, isMetallic: true)
+                    let textEntity = ModelEntity(mesh: textMesh, materials: [textMaterial])
+                    let anchorPalpiteText = AnchorEntity(.plane(.horizontal, classification: [.floor, .table], minimumBounds: [0.2, 0.2]))
+                    textEntity.scale = SIMD3<Float>(x: 0.03, y: 0.03, z: 0.1)
+                    textEntity.generateCollisionShapes(recursive: true)
+                    textEntity.name = "\(index)"
+                    anchorTextPalpite.addChild(textEntity)
+                }
+                
+
+            }
+
+            //anchorTextPalpite.removeChild(anchorTextPalpite.children[0])
+            
+        }
+
     }
     
     //from UIKit to SwifUi
     func makeCoordinator() -> Coordinator {
         return Coordinator(palpites: $palpites)
     }
+    
+    
     
     class Coordinator: NSObject{
         @Binding var palpites: [Int]
